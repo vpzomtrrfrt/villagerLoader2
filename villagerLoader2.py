ebea57b3e698a7c89886083063933a8aebb8aaed
Colin Reeder <vpzomtrrfrt@gmail.com>
2016-04-11 17:31:32 -0600

    Arguments

--- villagerLoader2.py
+++ villagerLoader2.py
@@ -1,9 +1,33 @@
-import vl2, random
-#vl2.load("takeout-20160408T184318Z.zip")
-#vl2.load("vl2.py")
-d = vl2.load("Sdubzskycraft-0.73.zip")
-for x in d:
-	if vl2.shouldDownloadFile(x, True):
-		name = vl2.downloadFile(x)
-		if name != None:
-			print("Downloaded "+name)
+import vl2, json, argparse
+parser = argparse.ArgumentParser(description="A Minecraft modpack downloader")
+parser.add_argument('source', nargs="?")
+args = parser.parse_args()
+src = None
+cfgFile = vl2._datadir+"/config.json"
+try:
+	config = json.load(open(cfgFile, 'r'))
+except FileNotFoundError:
+	config = {}
+if args.source == None:
+	if "source" in config:
+		src = config["source"]
+else:
+	src = args.source
+if src == None:
+	print("You must specify a pack source.")
+else:
+	save = True
+	if "://" in src:
+		to = vl2.OpenableURL(src)
+	else:
+		to = vl2.OpenableFile(src)
+		save = False
+	d = vl2.load(to)
+	config["source"] = src
+	vl2._mkparents(cfgFile)
+	json.dump(config, open(cfgFile, 'w'))
+	for x in d:
+		if vl2.shouldDownloadFile(x, True):
+			name = vl2.downloadFile(x)
+			if name != None:
+				print("Downloaded "+name)
